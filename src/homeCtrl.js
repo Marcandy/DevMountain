b833b24646d6aa0fce51d6259f87ea06b9e167d6
marcandy <marcandy.nj@gmail.com>
2016-09-08 11:12:14 -0500

    working next page

--- src/homeCtrl.js
+++ src/homeCtrl.js
@@ -1,6 +1,19 @@
 angular.module('Starship')
 .controller('homeCtrl', function ($scope, starshipService) {
 
+  $scope.characters = [];
 
+  $scope.getCharacters = function() {
+    starshipService
+      .getCharacters()
+      .then(function (characters) {
+        console.log(characters)
+        $scope.characters = characters.data.results;
+      })
+      .catch(function (error) {
+        console.error('something broke' + error);
+      });
+  }
+  $scope.getCharacters();
 
 });
--- src/starshipService.js
+++ src/starshipService.js
@@ -1,6 +1,18 @@
 angular.module('Starship')
 .service('starshipService', function ($http) {
+  var baseUrl = 'http://swapi.co/api/';
+  var nexPage = '';
 
+  this.getCharacters = function () {
+    if (nexPage) {
+      return $http.get(nexPage)
+      .then(saveNextPage);
+    }
+    return $http.get(baseUrl + 'people').then(saveNextPage);
+  }
 
-
+  function saveNextPage(result) {
+    nexPage = result.data.next;
+    return result;
+  }
 });
